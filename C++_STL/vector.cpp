4f5cd672116cec301b94b3e08551a137089edefb
suraj <surajbanthot@gmail.com>
2020-08-30 17:19:06 +0530

    vector template of C++ STL

--- C++_STL/vector.cpp
+++ C++_STL/vector.cpp
@@ -5,45 +5,56 @@
 
 using namespace std;
 
-bool f(int x, int y)
-{
-    return x > y;
-}
+  bool f(int x, int y)
+    {
+        return x > y;
+    }
 
-int main()
+void vectorDemo()
 {
     //C++ STL
-    vector<int> A = {11, 2, 3, 4};
-    // cout<<A[1]<<endl;
+    // vector<int> A = {11, 2, 3, 4};
+    // // cout<<A[1]<<endl;
 
-    sort(A.begin(), A.end()); //O(NlogN)
+    // sort(A.begin(), A.end()); //O(NlogN)
 
-    for (auto x : A)
-        cout << x << endl;
+    // for (auto x : A)
+    //     cout << x << endl;
 
-    cout << "**********\n";
+    // cout << "**********\n";
 
-    // bool present =binary_search(A.begin(),A.end(),3);
+    // // bool present =binary_search(A.begin(),A.end(),3);
 
-    // if(present)cout<<"present";
+    // // if(present)cout<<"present";
 
-    A.push_back(100);
+    // A.push_back(100);
 
-    for (auto x : A)
-        cout << x << endl;
+    // for (auto x : A)
+    //     cout << x << endl;
 
-    A.push_back(100);
-    A.push_back(123);
+    // A.push_back(100);
+    // A.push_back(123);
 
-    //2,3,4,11,100,100,123
+    // //2,3,4,11,100,100,123
 
-    vector<int>::iterator itrlower = lower_bound(A.begin(), A.end(), 100); // Gives the first element >= to the specified
-    vector<int>::iterator itrupper = upper_bound(A.begin(), A.end(), 100); // Gives the first element > to the specified
-    cout << "lower_bound" << *itrlower << endl;
-    cout << "upper_bound" << *itrupper << endl;
+    // vector<int>::iterator itrlower = lower_bound(A.begin(), A.end(), 100); // Gives the first element >= to the specified
+    // vector<int>::iterator itrupper = upper_bound(A.begin(), A.end(), 100); // Gives the first element > to the specified
+    // cout << "lower_bound" << *itrlower << endl;
+    // cout << "upper_bound" << *itrupper << endl;
 
-    cout << itrupper - itrlower << endl;
+    // cout << itrupper - itrlower << endl;
 
-    sort(A.begin(), A.end(), f);
-    return 0;
+    //to print the elements in descending order
+
+   
+    // sort(A.begin(), A.end(), f);
+
+   
+    // //normal iteration
+    // // for (auto c : A)
+    // //     cout << c << endl;
+    // //increasing the value of the elements of the array by one using reference
+
+    // for(auto &c: A)
+    // cout<<++c<<endl; 
 }
