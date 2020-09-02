bb8f77b9661625759d46b960d80ed36233f23649
suraj <surajbanthot@gmail.com>
2020-09-02 14:25:39 +0530

    Taylor_series: Horner's, Iterative, Recursive

--- Taylor_series.cpp
+++ Taylor_series.cpp
@@ -2,23 +2,59 @@
 
 using namespace std;
 
+// double e(int x, int n)
+// {
+//     static double p = 1, f = 1;
+//     double r;
+//     if (n == 0)
+//         return 1;
+//     r = e(x, n - 1);
+//     p = p * x;
+//     f = f * n;
+//     return r + (p / f);
+// }
+
+// double e(int x, int n)
+// {
+//     double s = 1;
+//     for (; n > 0; n--)
+//     {
+//         s = 1 + x * s / n;
+//     }
+//     return s;
+// }
+
+// double e(int x, int n)
+// {
+//     static double s=1;
+//     if(n==0)
+//     return s;
+//     s=1+x*s/n;
+//     return e(x,n-1);
+// }
+
+//iterative
 double e(int x, int n)
 {
-    static double p = 1, f = 1;
-    double r;
-    if (n == 0)
-        return 1;
-    r = e(x, n - 1);
-    p = p * x;
-    f = f * n;
-    return r + (p / f);
+    double s = 1;
+    int i;
+    double num = 1;
+    double den = 1;
+
+    for (i = 1; i <= n; i++)
+    {
+        num *= x;
+        den *= i;
+        s = num / den;
+    }
+    return s;
 }
 
 int main()
 {
-    
+
     double result;
-    result=e(1,10);
+    result = e(1, 10);
     cout << "Series " << result << endl;
 
     return 0;
