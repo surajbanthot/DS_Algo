88af118e10bd2ab15327be47960a22365b2626b3
suraj <surajbanthot@gmail.com>
2020-09-07 03:37:58 +0530

    LInked_list/GFG_linklist:

--- LInked_list/GFG_linklist.cpp
+++ LInked_list/GFG_linklist.cpp
@@ -1,6 +1,4 @@
-#include<iostream>
-#include<stdlib.h>
-#include<stdio.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -9,7 +7,11 @@ class Node
     public:
     int data;
     Node *next;
+};
 
+class LinkedList
+{
+    public:
     void push(Node** head_ref, int new_data)
     {
         Node* new_node=new Node();
@@ -54,12 +56,47 @@ class Node
         last->next=new_node;
         return;
     }
+
+    void printList(Node *node)
+    {
+        while(node!=NULL)
+        {
+            cout<<" "<<node->data;
+            node=node->next;
+        }
+    }
+
 };
 
 
+int main()  
+{  
 
-int main()
-{
-
-return 0;
-}
+    LinkedList l;
+    /* Start with the empty list */
+    Node* head = NULL;  
+      
+    // Insert 6. So linked list becomes 6->NULL  
+    l.append(&head, 6);  
+      
+    // Insert 7 at the beginning.  
+    // So linked list becomes 7->6->NULL  
+    l.push(&head, 7);  
+      
+    // Insert 1 at the beginning.  
+    // So linked list becomes 1->7->6->NULL  
+    l.push(&head, 1);  
+      
+    // Insert 4 at the end. So  
+    // linked list becomes 1->7->6->4->NULL  
+    l.append(&head, 4);  
+      
+    // Insert 8, after 7. So linked  
+    // list becomes 1->7->8->6->4->NULL  
+    l.insertAfter(head->next, 8);  
+      
+    cout<<"Created Linked list is: ";  
+    l.printList(head);  
+      
+    return 0;  
+}  
