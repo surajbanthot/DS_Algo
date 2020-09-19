933581bcff37ef31fb37785cafbd87fd970a27c7
suraj <surajbanthot@gmail.com>
2020-09-19 12:43:14 +0530

    BST Search, Recursive Insertion, Deletion

--- tree/BST.c
+++ tree/BST.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 struct Node
 {
@@ -7,7 +7,7 @@ struct Node
     int data;
     struct Node *rchild;
 }
-*root = NULL;
+    *root = NULL;
 
 void Insert(int key)
 {
@@ -19,13 +19,13 @@ void Insert(int key)
         p = (struct Node *)malloc(sizeof(struct Node));
         p->data = key;
         p->lchild = p->rchild = NULL;
-        root=p;
-        return; 
+        root = p;
+        return;
     }
     while (t != NULL)
     {
         r = t;
-        if (key < t->data)S
+        if (key < t->data)
             t = t->lchild;
         else if (key > t->data)
             t = t->rchild;
@@ -47,9 +47,83 @@ void Insert(int key)
     }
 }
 
+struct Node *RInsert(struct Node *p, int key)
+{
+    struct Node *t = NULL;
+    if (p == NULL)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = key;
+        t->lchild = t->rchild = NULL;
+        return t;
+    }
+    if (key < p->data)
+        p->lchild = RInsert(p->lchild, key);
+    else if (key > p->data)
+        p->rchild = RInsert(p->rchild, key);
+    return p;
+}
+int Height(struct Node *p)
+{
+    int x, y;
+    if (p == NULL)
+        return 0;
+    x = Height(p->lchild);
+    y = Height(p->rchild);
+    return x > y ? x + 1 : y + 1;
+}
+struct Node *InPre(struct Node *p)
+{
+    while (p && p->rchild != NULL)
+        p = p->rchild;
+
+    return p;
+}
+struct Node *InSucc(struct Node *p)
+{
+    while (p && p->lchild != NULL)
+        p = p->lchild;
+
+    return p;
+}
+struct Node *Delete(struct Node *p, int key)
+{
+    struct Node *q;
+
+    if (p == NULL)
+        return NULL;
+    if (p->lchild == NULL && p->rchild == NULL)
+    {
+        if (p == root)
+            root = NULL;
+        free(p);
+        return NULL;
+    }
+
+    if (key < p->data)
+        p->lchild = Delete(p->lchild, key);
+    else if (key > p->data)
+        p->rchild = Delete(p->rchild, key);
+    else
+    {
+        if (Height(p->lchild) > Height(p->rchild))
+        {
+            q = InPre(p->lchild);
+            p->data = q->data;
+            p->lchild = Delete(p->lchild, q->data);
+        }
+        else
+        {
+            q = InSucc(p->rchild);
+            p->data = q->data;
+            p->rchild = Delete(p->rchild, q->data);
+        }
+    }
+    return p;
+}
 void Inorder(struct Node *p)
 {
-    if(p)
+    if (p)
     {
         Inorder(p->lchild);
         printf("%d ", p->data);
@@ -57,16 +131,47 @@ void Inorder(struct Node *p)
     }
 }
 
+struct Node *Search(int key)
+{
+    struct Node *t = root;
+
+    while (t != NULL)
+    {
+        if (key == t->data)
+            return t;
+        else if (key < t->data)
+            t = t->lchild;
+        else
+            t = t->rchild;
+    }
+    return NULL;
+}
+
 int main()
 {
+    root = RInsert(root, 3);
+    RInsert(root, 4);
     Insert(10);
     Insert(5);
     Insert(20);
     Insert(8);
     Insert(30);
 
+    Delete(root, 5);
+    Inorder(root);
+    printf("\n");
+
     Inorder(root);
 
+    struct Node *temp;
+
+    temp = Search(8);
+
+    if(temp!=NULL)
+    printf("element %d is found\n", temp->data);
+    else
+    printf("element is not found \n");
+    
     printf("\n");
-    return 0; 
+    return 0;
 }
