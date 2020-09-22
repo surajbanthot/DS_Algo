2516d773be9c4f04fc7fb6caafa7140947eb05bc
suraj <surajbanthot@gmail.com>
2020-09-22 10:42:18 +0530

    Sort/shell:

--- Sort/shell.c
+++ Sort/shell.c
@@ -30,7 +30,7 @@ int main()
 {
     int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3}, n = 10, i;
 
-    SellSort(A, n);
+    ShellSort(A, n);
 
     for (i = 0; i < 10; i++)
         printf("%d ", A[i]);
